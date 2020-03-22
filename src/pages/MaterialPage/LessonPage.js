29b772c037325ed35f1f5658fc8883dd2d640cef
Lucas Turci <lucas.turci@gmail.com>
2020-03-22 16:17:52 -0300

    WIP put markdown lessons in site

--- src/pages/MaterialPage/LessonPage.js
+++ src/pages/MaterialPage/LessonPage.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import Markdown from 'react-markdown'
+import axios from 'axios'
+import CodeBlock from './CodeBlock'
 
 import './style.css';
+import './reset-this.css'
 
 // xs={12} sm={12} md={12} lg={12} xl={12}
 function LessonPage() {
     const { goBack } = useHistory()
+    const { title } = useParams()
+    const [sourceText, setSourceText] = useState("## Carregando texto...")
+    useEffect(() => {
+        const path = require(`./lessons/${title}.md`)
+        axios.get(path, {responseType: 'text'}).then(response => {
+
+            // Troca barras invertidas (que o markdown nao sabe parsear) pra <br/>
+            response.data = response.data.replace(/\\[\r\n]/g, "<br/>")
+            setSourceText(response.data)
+        })
+    }, [])
+    
 	return (
 		<div id="main-material">
 			<Container fluid id="cont">
@@ -27,8 +44,9 @@ function LessonPage() {
                 <div class="grow padding-top-2">
                     <Row>
                         <Col sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}>
-
-                            Placeholder
+                            <div class='reset-this'>
+                                <Markdown source={sourceText} escapeHtml={false} renderers={{code: CodeBlock}}/>
+                            </div>
                         </Col>
                     </Row>
                 </div>
