c7744893abf86a88c1ca0433664b7ada25fb431f
yasmin_araujo <yasminoa@hotmail.com>
2020-04-06 13:29:45 -0300

    Update links distribution at MaterialPage

--- src/pages/MaterialPage/index.js
+++ src/pages/MaterialPage/index.js
@@ -11,16 +11,26 @@ import './style.css';
 
 // xs={12} sm={12} md={12} lg={12} xl={12}
 function MaterialPage() {
-    const history = useHistory();
-    let classes = [];
-    
-    // Para adicionar novos links de aulas insira no final do array classes conforme o formato a seguir
+	const history = useHistory();
+	let classes = [];
+	let links = [];
+
+	// Para adicionar novos links de aulas insira no final do array classes conforme o formato a seguir
 	classes.push({ title: 'Introdução', url: '/Material/Aula/Introducao' });
 	classes.push({ title: 'Programação C/C++', url: '/Material/Aula/Programacao_C-C++' });
 	classes.push({ title: 'Repetição', url: '/Material/Aula/Repeticao' });
 	classes.push({ title: 'Arrays e Strings', url: '/Material/Aula/Arrays_Strings' });
 	classes.push({ title: 'Funções e Recursão', url: '/Material/Aula/Funcoes_Recursao' });
 	// classes.push({ title: 'Nome da aula', url: 'Material/Aula/url da aula' });
+    
+    // Links de materiais relevantes
+    links.push({ title: 'GitHub do GEMA', url: 'https://github.com/icmcgema' });
+    links.push({ title: 'GitHub do Fakhoury', url: 'https://github.com/andrefakhoury' });
+    links.push({ title: 'GitHub do Forbes', url: 'https://github.com/VictorXjoeY' });
+    links.push({ title: 'GitHub do LoppA', url: 'https://github.com/LoppA' });
+    links.push({ title: 'GitHub da lmoura', url: 'https://github.com/lusmoura' });
+    links.push({ title: 'GitHub do Preischadt', url: 'https://github.com/thiagop-usp' });
+    links.push({ title: 'GitHub do David', url: 'https://github.com/davidcairuz' });
 
 	return (
 		<div id="main-material">
@@ -50,7 +60,7 @@ function MaterialPage() {
 				<Row>
 					<Col sm={{ span: 5, offset: 1 }} xs={{ span: 10, offset: 1 }}>
 						<ListGroup className="material-links first">
-							{classes.slice(0, Math.ceil(classes.length/2)).map((c) => (
+							{classes.slice(0, Math.ceil(classes.length / 2)).map((c) => (
 								<ListGroup.Item action onClick={() => history.push(c.url)}>
 									{c.title}
 								</ListGroup.Item>
@@ -59,7 +69,7 @@ function MaterialPage() {
 					</Col>
 					<Col sm={{ span: 5, offset: 0 }} xs={{ span: 10, offset: 1 }}>
 						<ListGroup className="material-links">
-							{classes.slice(Math.ceil(classes.length/2), classes.lastIndex).map((c) => (
+							{classes.slice(Math.ceil(classes.length / 2), classes.lastIndex).map((c) => (
 								<ListGroup.Item action onClick={() => history.push(c.url)}>
 									{c.title}
 								</ListGroup.Item>
@@ -67,35 +77,31 @@ function MaterialPage() {
 						</ListGroup>
 					</Col>
 				</Row>
-				<Row>
-					<Col id="links-col" sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}>
+                <Row>
+					<Col sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}>
 						<hr />
-						<h5>Links relevantes</h5>
-						<ListGroup variant="flush">
-							<ListGroup.Item action href="https://github.com/icmcgema">
-								GitHub do GEMA
-							</ListGroup.Item>
-							<ListGroup.Item action href="https://github.com/andrefakhoury">
-								GitHub do Fakhoury
-							</ListGroup.Item>
-							<ListGroup.Item action href="https://github.com/VictorXjoeY">
-								GitHub do Forbes
-							</ListGroup.Item>
-							<ListGroup.Item action href="https://github.com/LoppA">
-								GitHub do LoppA
-							</ListGroup.Item>
-							<ListGroup.Item action href="https://github.com/lusmoura">
-								GitHub da lmoura
-							</ListGroup.Item>
-							<ListGroup.Item action href="https://github.com/thiagop-usp">
-								GitHub do Preischadt
-							</ListGroup.Item>
-							<ListGroup.Item action href="https://github.com/davidcairuz">
-								GitHub do David
-							</ListGroup.Item>
+						<h4>Links relevantes</h4>
+					</Col>
+					<Col className="links-col" sm={{ span: 5, offset: 1 }} xs={{ span: 10, offset: 1 }}>
+						<ListGroup className="material-links first">
+							{links.slice(0, Math.ceil(links.length / 2)).map((l) => (
+								<ListGroup.Item action href={l.url} rel="noopener noreferrer" target="_blank">
+									{l.title}
+								</ListGroup.Item>
+							))}
 						</ListGroup>
 					</Col>
-				</Row>
+					<Col className="links-col" sm={{ span: 5, offset: 0 }} xs={{ span: 10, offset: 1 }}>
+						<ListGroup className="material-links">
+							{links.slice(Math.ceil(links.length / 2), links.lastIndex).map((l) => (
+								<ListGroup.Item action href={l.url} rel="noopener noreferrer" target="_blank">
+									{l.title}
+								</ListGroup.Item>
+							))}
+						</ListGroup>
+					</Col>
+                    </Row>
+				
 				<Row className="footer-row">
 					<Footer />
 				</Row>
