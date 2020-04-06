aa1efaafea65ca575c3f616ac9a49073b25c670b
yasmin_araujo <yasminoa@hotmail.com>
2020-04-06 12:49:41 -0300

    Update routes with folder change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import HomePage from './pages/HomePage/index';
 import AboutPage from './pages/AboutPage/index';
 import MaterialPage from './pages/MaterialPage/index';
@@ -10,9 +11,10 @@ import FaqPage from './pages/FaqPage/index';
 import NewsPage from './pages/NewsPage/index';
 import ContactPage from './pages/ContactPage/index';
 import ContestPage from './pages/ContestPage/index';
+import LessonPage from './pages/LessonPage/index';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route } from 'react-router'
-import LessonPage from './pages/MaterialPage/LessonPage'
 
 function App(){
     return (
